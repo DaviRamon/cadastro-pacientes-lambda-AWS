6a581fdd757ffc4d9e1d1cb4d7f125084a940d48
Davi Ramon <eudavi.ramon@gmail.com>
2022-02-10 13:13:14 -0300

    criado funcao e feito deploy de obterPaciente

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const pacientes= [
-  
+
   {id: 1, nome: "Davi", idade: 32},
   {id: 2, nome: "Bruna", idade: 26},
   {id: 3, nome: "Betoven", idade: 7},
@@ -24,3 +24,19 @@ module.exports.listarPacientes = async (event) => {
   };
 
 };
+
+module.exports.obterPaciente = async (event) => {
+  console.log(event); //recebe as informações da req
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        pacientes
+      },
+      null,
+      2
+    ),
+  };
+
+};
+
